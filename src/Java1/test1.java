a22e135edc8cff407db3cf1ebc8a51efe11c7f2b
alumno <alumno@DESKTOP-UE84JL7>
2022-11-14 21:08:45 +0100

    First commit

---
+++ src/Java1/test1.java
@@ -0,0 +1,19 @@
+package Java1;
+
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Escribe numero:");
+		int n1 = entrada.nextInt();
+		
+		System.out.println(n1);
+		
+	}
+
+}
